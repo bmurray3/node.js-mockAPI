12e34a806459dcc984012c9052f6ef83b4a33cd3
Brian Murray <brianm@azfog.vdi>
2019-03-12 18:43:52 -0400

    origin master

--- functions/functions.js
+++ functions/functions.js
@@ -1,33 +1,26 @@
-const express = require('express');
-const router = express.Router();
-
-
-router.post('/sku/:skuid/build/create', (req, res, next) => {
-    sku.find({"_id": req.params.skuid}).then(data => {
-        console.log(data[0]);
-        let obj = data[0]["_doc"];
-        let newObj = {};
-        for(let key in obj) {
-            if(key !== "_id" && key !== "__v") {
-                newObj[key] = obj[key];
-            }
-        }
-        res.json(newObj);
-    }).catch(next);
-});
-
-
-//start of function to do above. Then get rid of above once it works.
-
 var exports = module.exports = {};
 
-exports.transposeIdAs = function(data, strip, add) {
+exports.transposeKey = function(data, strip, add) {
     let jsonIn = data[0]["_doc"];
     let jsonOut = {};
     for(attr in jsonIn) {
         if(key == strip) {
-            key = add
+            jsonOut[key] = add
+        }else {
+            jsonOut[key] = jsonIn[key]
         }
-        jsonOut 
+        
     }
+    res.json(jsonOut)
+};
+
+exports.removeKey = function(data, strip) {
+    let jsonIn = data[0]["_doc"];
+    let jsonOut = {};
+    for(attr in jsonIn) {
+        if(key !== strip) {
+            jsonOut[key] = jsonIn[key]
+        }
+    }
+    res.json(jsonOut)
 };
--- routes/compose.js
+++ routes/compose.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const sku = require('../schema/sku');
 const build = require('../schema/build');
 const component = require('../schema/component');
-const _ = require('lodash');
+const functions = require('../functions/functions')
 
 //Query all SKUs
 router.get('/sku', (err, res, next) => {
@@ -15,48 +15,15 @@ router.get('/sku/:skuid/status', (err, res, next) => {
     sku.findById(req.params.skuid).then(data => res.json(data)).catch(next)
 });
 
-//Mike's start 
-// Initiate new build based on specific SKU
-// router.post('/sku/:skuid/build/create', (req, res, next) => {
-//     sku.find({"_id": req.params.skuid}).then(data => {
-//         res.json(data)}).catch(next);
-//     //ok so how do I add my own json attributes to the body now? 
-//     build.create(req.body).then(data => res.json(data)).catch(next)
-// });
-
-// Where I was heading I think...
 //Initiate new build based on specific SKU
 router.post('/sku/:skuid/build/create', (req, res, next) => {
     sku.find({"_id": req.params.skuid})
     .then(data => {
-
-        let d = data[0];
-        console.log(d);
-        let blah = _.remove(d, {
-            "_id": d["_id"]
-        })
-       
-        console.log(blah);
-
-
-        
-        // delete d['skuName'];
-        
-        // console.log(d);
-                
-
-
-        // data["_id"] = "kuhsrdgkhaet98y3q4yigsef";
-        // data["super"] = "newnewnwewnewnewnewnew";
-        // let updatedData = Object.assign(data, { myNewProp: "hiya" });
-        // //Got a specific SKU. now to clone it as a new BUILD
-        // build.create(updatedData);
-        // build.create('my found SKU record detail, but we are creating new _id for the build and inserting the sku _id as buildSkuReference atrribute');
-        res.json('build create data');
+        functions.removeKey(data, '__v');
+        functions.transposeKey(data, '_id', 'buildSkuReference')
     })
     .catch(next);
 });
-
 
 //Query all builds associated with specific SKU
 router.get('/sku/:skuid/build', (err, res, next) => {
